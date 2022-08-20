9c1e2893cd74cf8831902faba73cf5a67e21b279
elena-solomakhin <zabara.lena@gmail.com>
2022-08-20 15:16:33 +0300

    unsuccess

--- src/test/java/contacts/LoginTests.java
+++ src/test/java/contacts/LoginTests.java
@@ -25,5 +25,50 @@ OkHttpClient client =new OkHttpClient();
  Response response= client.newCall(request).execute();
         Assert.assertTrue(response.isSuccessful());
     }
+    @Test
+    public void loginUnsuccess1() throws IOException {
+        AuthRequestDto auth= AuthRequestDto.builder().email("miagmail.com").password("Mmia12345$").build();
+        RequestBody requestBody= RequestBody.create(gson.toJson(auth),JSON);
+
+        Request request= new Request.Builder()
+                .url("https://contacts-telran.herokuapp.com/api/login")
+                .post(requestBody)
+                .build();
+
+       Response response= client.newCall(request).execute();
+       Assert.assertFalse(response.isSuccessful());
+       Assert.assertEquals(response.code(),400);
+
+    }
+    @Test
+    public void loginUnsuccess2() throws IOException {
+        AuthRequestDto auth= AuthRequestDto.builder().email("noa@gmail.com").password("Mmia12345$").build();
+        RequestBody requestBody= RequestBody.create(gson.toJson(auth),JSON);
+
+        Request request= new Request.Builder()
+                .url("https://contacts-telran.herokuapp.com/api/login")
+                .post(requestBody)
+                .build();
+
+        Response response= client.newCall(request).execute();
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(),401);
+
+    }
+    @Test
+    public void loginUnsuccess3() throws IOException {
+        AuthRequestDto auth= AuthRequestDto.builder().email("noa@gmail.com").password("Mm").build();
+        RequestBody requestBody= RequestBody.create(gson.toJson(auth),JSON);
+
+        Request request= new Request.Builder()
+                .url("https://contacts-telran.herokuapp.com/api/login")
+                .post(requestBody)
+                .build();
+
+        Response response= client.newCall(request).execute();
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(),400);
+
+    }
 
 }
